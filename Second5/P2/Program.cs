8a67e58bf9bd2f0a81566fac8bef20acd3941940
Сергей <Sergy87@yandex.ru>
2023-01-31 23:03:44 +0300

    add Second5

---
+++ Second5/P2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 32: Напишите программу замена элементов массива:
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+Алгоритм решения:
+1. Инициализируем массив
+2. Заполняем массив случайными числами
+3. Переворачиваем числа
+4. Выводим на экран
+*/
+
+int[] InitArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+void ConvertArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+int[] array = InitArray(4);
+Console.WriteLine(string.Join(",", array));
+
+ConvertArray(array);
+Console.WriteLine(string.Join(",", array));
---
+++ Second5/P3/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 33: Задайте массив. Напишите программу, которая определяет,
+присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+int[] InitArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+bool FindNumber(int find, int[] arr)
+{
+    bool result = false;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (find == arr[i])
+        {
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+int[] arr = InitArray(10);
+Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine("Что будем искать? ");
+int find = Convert.ToInt32(Console.ReadLine());
+bool a = FindNumber(find, arr);
+
+if (a) Console.WriteLine("Элемент найден");
+else Console.WriteLine("Элемент не найден");
