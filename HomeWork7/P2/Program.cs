ccb092fcc643c18d1faf88910886b7b75c2a2c9f
Сергей <Sergy87@yandex.ru>
2023-02-10 00:09:49 +0300

    add T2

--- HomeWork7/P2/Program.cs
+++ HomeWork7/P2/Program.cs
@@ -12,27 +12,6 @@
 пользователь вводите индексы 0, 2 - выводим элеменет 7
 */
 
-int GetNumber(string message)
-{
-    int result = 0;
-
-    while (true)
-    {
-        Console.WriteLine(message);
-
-        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
-        }
-
-    }
-    return result;
-}
-
 int[,] InitMatrix(int rows, int columns)
 {
     int[,] matrix = new int[rows, columns];
@@ -60,3 +39,24 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
+
+int[,] matrix = InitMatrix(3, 4);
+
+PrintMatrix(matrix);
+
+Console.WriteLine("Введите индекс строки");
+int rows = Convert.ToInt32(Console.ReadLine()) - 1;
+
+Console.WriteLine("Введите индекс столбца");
+int columns = Convert.ToInt32(Console.ReadLine()) - 1;
+
+if (rows > 0 && rows < matrix.GetLength(0) && columns > 0 && columns < matrix.GetLength(1))
+{
+    Console.WriteLine($"Число на данной позиции :{matrix[rows, columns]}");
+}
+
+else
+{
+    Console.WriteLine("такого элемента нет");
+}
+
